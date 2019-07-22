f98c94e0fd8ff584a54ba05bb7a09872f951c58b
Jason Paul Benson <44947694+jasonpbenson@users.noreply.github.com>
2019-07-22 19:46:04 -0400

    removed bad gatsby-source-filesystem path

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,12 +21,6 @@ module.exports = {
         name: "pages",
       },
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/images`,
-        name: "images",
-      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
