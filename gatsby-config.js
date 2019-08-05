8babca910ba37c6f2277dcd893a6bfec69684b5f
jasonpbenson <jasonbenson3rd@gmail.com>
2019-08-04 21:06:41 -0400

    reset

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [`gatsby-plugin-netlify-cms`],
+  /* Your site config here */
+}
